afbcc8f7101f0fc626cecca38d5ff8f12620906d
Chattriya Jariyavajee <jetstream.c@gmail.com>
2019-02-02 23:24:58 +0700

    Maker Playground Code runner for RPi

---
+++ index.js
@@ -0,0 +1,77 @@
+const child_process = require('child_process')
+const fs = require('fs')
+const rimraf = require('rimraf')
+const unzipper = require('unzipper')
+const express = require('express')
+const fileUpload = require('express-fileupload')
+
+const port = 6212
+
+const scriptDir = __dirname + '/script'
+const scriptEntryFile = __dirname + '/script/main.py'
+const scriptZipFile = __dirname + '/script.zip'
+
+var proc = null
+var running = false
+
+function stopScript() {
+    if (running) {
+        proc.kill('SIGINT')
+        running = false
+    }
+}
+
+function runScript() {
+    if (fs.existsSync(scriptEntryFile) && !running) {
+        proc = child_process.spawn('python3', [scriptEntryFile])
+        running = true
+        proc.stdout.on('data', (data) => {
+            console.log(`${data}`)
+        })
+        proc.stderr.on('data', (data) => {
+            console.log(`${data}`)
+        })
+        proc.on('close', (retVal) => {
+            console.log(`${retVal}`)
+        })
+    }
+}
+
+runScript()
+
+const app = express()
+
+app.use(fileUpload())
+
+app.get('/', (req, res) => {
+    res.send('makerplayground')
+})
+
+app.post('/upload', (req, res) => {
+    if (Object.keys(req.files).length == 0 || !req.files.script) {
+        res.status(400).send('No files were uploaded.')
+    }
+
+    let extractor = unzipper.Extract({path: __dirname})
+    .on('close', () => {
+        stopScript()
+        runScript()
+        console.log(proc.pid)
+        res.send('File uploaded!')
+        rimraf(scriptZipFile, () => {})
+    })
+    .on('error', (err) => {
+        if (err) return res.status(500).send(err)
+    })
+
+    req.files.script.mv(scriptZipFile, (err) => {
+        if (err) return res.status(500).send(err)
+        rimraf(scriptDir, () => {
+            fs.createReadStream(scriptZipFile)
+               .pipe(extractor)
+        })
+    })
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
+
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "installing nodejs 10.x"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt-get install -y nodejs
+echo "done"
+
+
+echo "installing node_modules"
+npm install
+echo "done"
+
+
+echo "registering makerplayground service"
+cp makerplayground.service /lib/systemd/system/makerplayground.service
+systemctl daemon-reload
+systemctl enable makerplayground.service
+echo "done"
